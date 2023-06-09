93462380e6373fec05c265e6673da3e760a6e7c6
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2023-06-09 13:36:09 -0400

    Auto setup.

---
+++ get_datasets.sh
@@ -0,0 +1,36 @@
+mkdir downloads
+cd downloads
+
+if test -f "minc-model.tar.gz"; then
+	echo "minc-model.tar.gz exist."
+else
+	wget http://opensurfaces.cs.cornell.edu/static/minc/minc-model.tar.gz
+	tar -xvf minc-model.tar.gz
+	mv minc-model/* ../datasets/models
+fi
+
+if test -f "minc-model.tar.gz"; then
+	echo "minc-model.tar.gz exist."
+else
+	wget http://opensurfaces.cs.cornell.edu/static/minc/minc-original-photo.tar.gz
+	tar -xvf minc-original-photo.tar.gz --directory ../datasets/
+	mv ../datasets/photo_prig ../datasets/minc
+fi
+
+if test -f "minc-model.tar.gz"; then
+	echo "minc-model.tar.gz exist."
+else
+	wget http://opensurfaces.cs.cornell.edu/static/minc/minc.tar.gz
+	tar -xvf minc.tar.gz 
+	mv minc/* ../datasets/minc
+fi
+
+if test -f "minc-model.tar.gz"; then
+	echo "minc-model.tar.gz exist."
+else
+	wget https://github.com/AhmedDhahri/Material_recognition/releases/download/datasets/Irh_dataset.zip
+	mkdir ../datasets/irh
+	mkdir datasets/irh/files
+	unzip Irh_dataset.zip -d datasets/irh/files
+fi
+
