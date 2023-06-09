edddef650c3f0d4c4ca3ffa5539926cd630185db
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2023-06-09 13:42:59 -0400

    Fix echos and tests of file names.

--- get_datasets.sh
+++ get_datasets.sh
@@ -9,24 +9,24 @@ else
 	mv minc-model/* ../datasets/models
 fi
 
-if test -f "minc-model.tar.gz"; then
-	echo "minc-model.tar.gz exist."
+if test -f "minc-original-photo.tar.gz"; then
+	echo "minc-original-photo.tar.gz exist."
 else
 	wget http://opensurfaces.cs.cornell.edu/static/minc/minc-original-photo.tar.gz
 	tar -xvf minc-original-photo.tar.gz --directory ../datasets/
 	mv ../datasets/photo_prig ../datasets/minc
 fi
 
-if test -f "minc-model.tar.gz"; then
-	echo "minc-model.tar.gz exist."
+if test -f "minc.tar.gz"; then
+	echo "minc.tar.gz exist."
 else
 	wget http://opensurfaces.cs.cornell.edu/static/minc/minc.tar.gz
 	tar -xvf minc.tar.gz 
 	mv minc/* ../datasets/minc
 fi
 
-if test -f "minc-model.tar.gz"; then
-	echo "minc-model.tar.gz exist."
+if test -f "Irh_dataset.zip"; then
+	echo "Irh_dataset.zip exist."
 else
 	wget https://github.com/AhmedDhahri/Material_recognition/releases/download/datasets/Irh_dataset.zip
 	mkdir ../datasets/irh
