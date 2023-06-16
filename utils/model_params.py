f8066ecc8ac2afac84f4197b0f211a875b702544
Ahmed Dhahri <ahmeddhahriuqtr.ca>
2023-06-15 21:08:56 -0400

    Fix bug typo.

--- utils/model_params.py
+++ utils/model_params.py
@@ -21,10 +21,10 @@ class model_params:
                 self.model = vit_h_14(weights=ViT_H_14_Weights.IMAGENET1K_SWAG_LINEAR_V1)
             self.size, self.bs = 224, 4
         elif  model_name == 'eva02l14':
-            self.model = timm.create_model('eva02_large_patch14_448.mim_m38m_ft_in1k', pretrained=load)
+            self.model = timm.create_model('eva02_large_patch14_448.mim_m38m_ft_in1k', pretrained=not load)
             self.size, self.bs = 448, 4
         elif  model_name == 'maxvitxl':
-            self.model = timm.create_model('maxvit_xlarge_tf_512.in21k_ft_in1k', pretrained=load)
+            self.model = timm.create_model('maxvit_xlarge_tf_512.in21k_ft_in1k', pretrained=not load)
             self.size, self.bs = 512, 2
         else:
             raise Exception("Sorry, model_name not valid!")
