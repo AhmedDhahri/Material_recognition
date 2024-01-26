d48928ba9af2582460da17f57eb16977efb980f4
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2024-01-26 14:36:18 +0100

    fix bug

--- train/train_irh.py
+++ train/train_irh.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append(sys.path[0]+'/../')
 sys.path.append(sys.path[0]+'/../utils')
 
-import timm
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -11,7 +10,6 @@ from torch.utils.data import DataLoader
 from dataloaders.irh_dataloader import IRHDataset
 from utils import CosineDecayLR, Metrics
 from model_params import model_params
-from timm.layers import SelectAdaptivePool2d
 from models.coatnet2_multimodal import coatnet_full
 
 
