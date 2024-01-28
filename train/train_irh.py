6ce276b1f20b132ca3b16677235edbc99ea913cb
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2024-01-28 09:55:56 +0100

    eval mode

--- train/train_irh.py
+++ train/train_irh.py
@@ -17,7 +17,9 @@ TRAIN_ITER = 500
 BATCH_SIZE, EPOCHS, EXPERIMENT, LOAD = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), True
 #model, _, log_file, SIZE, BATCH_SIZE = model_params(model_name=sys.argv[1], load=LOAD).get() #"swinv2b", "vith14", "eva02l14", "maxvitxl", coatnet2
 SIZE, LR  = 384, 4e-5
-model = coatnet_full(EXPERIMENT).cuda()
+model = coatnet_full(EXPERIMENT)
+model = model.eval()
+model = model.cuda()
 
 global net_name 
 if EXPERIMENT == 0:
