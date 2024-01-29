46014ce066487ea88de4a7649d894aea7093c7c1
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2024-01-29 23:33:12 +0100

    improve train loop. Update LR

--- train/train_irh.py
+++ train/train_irh.py
@@ -16,7 +16,7 @@ from models.coatnet2_multimodal import coatnet_full
 TRAIN_ITER = 500
 BATCH_SIZE, EPOCHS, EXPERIMENT, LOAD = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), True
 #model, _, log_file, SIZE, BATCH_SIZE = model_params(model_name=sys.argv[1], load=LOAD).get() #"swinv2b", "vith14", "eva02l14", "maxvitxl", coatnet2
-SIZE, LR  = 384, 5e-6
+SIZE, LR  = 384, 3e-6
 model = coatnet_full(EXPERIMENT)
 model = model.eval()
 model = model.cuda()
@@ -44,8 +44,8 @@ metric = Metrics()
 #One epoch done
 for epc in range(0, EPOCHS):
     ticket = "Epoch {}: ".format(epc)
-    r = tqdm((train_loader), leave=False, desc=ticket, total=len(train_loader))    
-    for X, Y in r:
+    r = tqdm(enumerate(train_loader), leave=False, desc=ticket, total=len(train_loader))    
+    for idx, (X, Y) in r:
         if EXPERIMENT == 0:
             x_rgb, x_nir, x_dpt = X, torch.Tensor(0), torch.Tensor(0)
         elif EXPERIMENT == 1:
@@ -60,8 +60,8 @@ for epc in range(0, EPOCHS):
         optimizer.step()
         optimizer.zero_grad()
         r.set_postfix(loss=lf.item())
-    #decrease lr
-    LR = lr_scheduler.step(epc * len(train_loader))
+        #decrease lr
+        LR = lr_scheduler.step(epc * len(train_loader)+ idx)
     with torch.no_grad():
         ac = 0
         for i in range(TRAIN_ITER):
